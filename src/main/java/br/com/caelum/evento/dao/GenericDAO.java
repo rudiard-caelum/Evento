2bc17e24c3dcd0b0bc295a9c73439e9b585b654b
rdv_consulting <r.rodrigues78@yahoo.com.br>
2017-03-15 12:57:50 -0300

    Alterado Tipo de Retorno do Metodo buscaId da Classe GenericDAO.

--- src/main/java/br/com/caelum/evento/dao/GenericDAO.java
+++ src/main/java/br/com/caelum/evento/dao/GenericDAO.java
@@ -3,37 +3,63 @@ package br.com.caelum.evento.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import br.com.caelum.evento.util.JPAUtil;
 
 @SuppressWarnings("rawtypes")
 public abstract class GenericDAO<T> {
 
 	private Class clazz;
-	private EntityManager manager;
+	private EntityManager manager = new JPAUtil().getEntityManager();
 
 	public GenericDAO() {
 	}
 
-	public GenericDAO(EntityManager manager, Class clazz) {
-		this.manager = manager;
+	public GenericDAO(Class clazz) {
 		this.clazz = clazz;
 
 	}
 
 	public void adiciona(T entity) {
+		this.manager.getTransaction().begin();
 		this.manager.persist(entity);
+		this.manager.getTransaction().commit();
 	}
 
 	public void altera(T entity) {
+		this.manager.getTransaction().begin();
 		this.manager.merge(entity);
+		this.manager.getTransaction().commit();
 	}
 
 	public void remove(T entity) {
+		this.manager.getTransaction().begin();
 		this.manager.remove(entity);
+		this.manager.getTransaction().commit();
 	}
 
 	@SuppressWarnings("unchecked")
-	public T busca(Integer id) {
+	public T buscaId(Long id) {
 		return (T) this.manager.find(clazz, id);
+	}
+
+	@SuppressWarnings("unchecked")
+	public Object buscaString(String valor, String campo) {
+		try {
+			CriteriaBuilder builder = this.manager.getCriteriaBuilder();
+			CriteriaQuery<T> criteria = builder.createQuery(this.clazz);
+			Root<T> from = criteria.from(this.clazz);
+			Predicate condition = builder.equal(from.get(campo), valor);
+			criteria.where(condition);
+			Object result = this.manager.createQuery(criteria).getSingleResult();
+			return result;
+		} catch (RuntimeException re) {
+			return null;
+		}
 	}
 
 	@SuppressWarnings("unchecked")
