5d2f691ba940bda345c0e0cd48b55337cb789825
rdv_consulting <r.rodrigues78@yahoo.com.br>
2017-03-24 14:00:42 -0300

    Criacao da Classe EntityManagerProducer.

---
+++ src/main/java/br/com/caelum/evento/util/EntityManagerProducer.java
@@ -0,0 +1,34 @@
+package br.com.caelum.evento.util;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
+@ApplicationScoped
+public class EntityManagerProducer {
+
+	@PersistenceUnit
+	private EntityManagerFactory emf;
+
+	@Produces
+	@RequestScoped
+	public EntityManager getEntityManager() {
+		try {
+			EntityManager manager = emf.createEntityManager();
+			return manager;
+		} catch (Exception | Error ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+
+	public void close(@Disposes EntityManager manager) {
+		if (manager != null) {
+			manager.close();
+		}
+	}
+
+}
