771aef0c35cf6b5a401e5e1c07c07a650293225a
rdv_consulting <r.rodrigues78@yahoo.com.br>
2017-03-27 12:52:30 -0300

    Refatoracao da Classe UsuarioDAOTest Para Utilizar o Entity Manager Para a Persistence-Unit TESTEDB.

--- src/test/java/br/com/caelum/evento/dao/UsuarioDAOTest.java
+++ src/test/java/br/com/caelum/evento/dao/UsuarioDAOTest.java
@@ -2,23 +2,57 @@ package br.com.caelum.evento.dao;
 
 import java.util.Random;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import br.com.caelum.evento.domain.Usuario;
 
 public class UsuarioDAOTest {
 
+	private static EntityManagerFactory emf;
+	private EntityManager manager;
+
 	private String nomeUsuario;
 	private boolean excluiUsuario = true;
 
 	private Usuario usuario = new Usuario();
 
-	private UsuarioDAO usuarioDAO = new UsuarioDAO();
+	private UsuarioDAO usuarioDAO;
+
+	@BeforeClass
+	public static void beforeClass() {
+		emf = Persistence.createEntityManagerFactory("testeDB");
+	}
+
+	@Before
+	public void before() {
+		this.manager = emf.createEntityManager();
+		this.manager.getTransaction().begin();
+		this.usuarioDAO = new UsuarioDAO(manager);
+	}
+
+	@After
+	public void after() {
+		this.manager.getTransaction().rollback();
+		this.manager.close();
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		emf.close();
+	}
 
 	private void setUsuario(Usuario usr) {
 		Random rdm = new Random();
-		Integer inteiro = rdm.nextInt();
+		Integer inteiro = rdm.nextInt(1000);
 		this.nomeUsuario = "TESTE_" + inteiro.toString();
 		usr.setNome(this.nomeUsuario);
 		usr.setEmail("teste@teste.com");
@@ -26,7 +60,6 @@ public class UsuarioDAOTest {
 	}
 
 	private void excluirUsuario() {
-		this.usuario = (Usuario) this.usuarioDAO.buscaString(this.nomeUsuario, "nome");
 		if (this.usuario != null) {
 			usuarioDAO.remove(this.usuario);
 		}
@@ -44,12 +77,12 @@ public class UsuarioDAOTest {
 	}
 
 	@Test
-	public void deveAlterarSenha() {
+	public void deveAlterarEmaildoUsuario() {
 		this.excluiUsuario = false;
 		this.deveInserirUsuario();
-		this.usuario.setSenha("123");
+		this.usuario.setEmail("teste@teste.com");
 		this.usuarioDAO.altera(this.usuario);
-		Assert.assertEquals("123", this.usuario.getSenha());
+		Assert.assertEquals("teste@teste.com", this.usuario.getEmail());
 		this.excluirUsuario();
 	}
 
@@ -62,7 +95,7 @@ public class UsuarioDAOTest {
 	}
 
 	@Test
-	public void deveBuscarId() {
+	public void deveBuscarIdUsario() {
 		this.excluiUsuario = false;
 		this.deveInserirUsuario();
 		Assert.assertNotNull(this.usuarioDAO.buscaId(this.usuario.getId()));
@@ -70,7 +103,7 @@ public class UsuarioDAOTest {
 	}
 
 	@Test
-	public void deveBuscarString() {
+	public void deveBuscarStringNomeUsuario() {
 		this.excluiUsuario = false;
 		this.deveInserirUsuario();
 		Assert.assertNotNull(this.usuarioDAO.buscaString(this.nomeUsuario, "nome"));
