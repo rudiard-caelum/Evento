2cca2e070b62aa1780e6fa79bd6398de8f42b38c
rdv_consulting <r.rodrigues78@yahoo.com.br>
2017-03-27 12:02:10 -0300

    Criacao da Classe NavigationHandlerUtil Para Injecao de FacesContext.

---
+++ src/main/java/br/com/caelum/evento/util/NavigationHandlerUtil.java
@@ -0,0 +1,21 @@
+package br.com.caelum.evento.util;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Produces;
+import javax.faces.application.NavigationHandler;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+public class NavigationHandlerUtil {
+
+	@Inject
+	FacesContext context;
+
+	@Produces
+	@RequestScoped
+	public NavigationHandler getNavigationHandler() {
+		return context.getApplication().getNavigationHandler();
+
+	}
+
+}
