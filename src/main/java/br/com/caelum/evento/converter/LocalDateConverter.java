068f6ce6337b2870bb89864c4ea0786c7b98a412
rdv_consulting <r.rodrigues78@yahoo.com.br>
2017-03-28 12:36:10 -0300

    Criacao do Converter LocalDateConverter Para Conversao do Campo Data da Classe Evento.

---
+++ src/main/java/br/com/caelum/evento/converter/LocalDateConverter.java
@@ -0,0 +1,39 @@
+package br.com.caelum.evento.converter;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
+
+import org.joda.time.LocalDate;
+
+import br.com.caelum.evento.domain.Evento;
+
+@FacesConverter(forClass = Evento.class)
+@RequestScoped
+public class LocalDateConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+		LocalDate ld = null;
+		try {
+			ld = LocalDate.fromDateFields(df.parse(value));
+		} catch (ParseException parseException) {
+			throw new ConverterException("Impossível converter a data.");
+		}
+		return ld;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		LocalDate ld = (LocalDate) value;
+		return ld.toString("dd/MM/yyyy");
+	}
+}
