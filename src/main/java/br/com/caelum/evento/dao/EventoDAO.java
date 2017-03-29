f72d8544e43d4818cf7d5ea5d9ab70ae71543024
rdv_consulting <r.rodrigues78@yahoo.com.br>
2017-03-29 00:41:43 -0300

    Inclusao do Metodo buscaPorNome() na Classe EventoDAO.

--- src/main/java/br/com/caelum/evento/dao/EventoDAO.java
+++ src/main/java/br/com/caelum/evento/dao/EventoDAO.java
@@ -1,7 +1,9 @@
 package br.com.caelum.evento.dao;
 
 import java.io.Serializable;
+import java.util.List;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 
 import br.com.caelum.evento.domain.Evento;
@@ -10,12 +12,22 @@ public class EventoDAO extends GenericDAO<Evento> implements Serializable {
 
 	private static final long serialVersionUID = -4976046817058319999L;
 
+	@Inject
+	private EntityManager manager;
+
 	public EventoDAO() {
 		super(Evento.class);
 	}
 
 	public EventoDAO(EntityManager manager) {
 		super(Evento.class, manager);
+	}
+
+	public List<Evento> buscaPorNome(String nome) {
+		String jpql = "select e from Evento e where lower(e.nome) like :nome order by e.nome";
+		List<Evento> lista = manager.createQuery(jpql, Evento.class)
+									.setParameter("nome", "%" + nome + "%").getResultList();
+		return lista;
 	}
 
 }
