9a4be10d241f125489010738caf8814f6bbb5e48
rdv_consulting <r.rodrigues78@yahoo.com.br>
2017-04-03 15:51:09 -0300

    Inclusao do Metodo rankingVotacaoPorPalestra na Classe PalestraDAO.

--- src/main/java/br/com/caelum/evento/dao/PalestraDAO.java
+++ src/main/java/br/com/caelum/evento/dao/PalestraDAO.java
@@ -1,6 +1,8 @@
 package br.com.caelum.evento.dao;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -9,6 +11,7 @@ import javax.persistence.TypedQuery;
 
 import br.com.caelum.evento.domain.Evento;
 import br.com.caelum.evento.domain.Palestra;
+import br.com.caelum.evento.domain.VotacaoEnum;
 
 public class PalestraDAO extends GenericDAO<Palestra> implements Serializable {
 
@@ -23,6 +26,7 @@ public class PalestraDAO extends GenericDAO<Palestra> implements Serializable {
 
 	public PalestraDAO(EntityManager manager) {
 		super(Palestra.class, manager);
+		this.manager = manager;
 	}
 
 	public Palestra getPalestra(Evento evento, String titulo) {
@@ -35,6 +39,48 @@ public class PalestraDAO extends GenericDAO<Palestra> implements Serializable {
 		} catch (NoResultException nre) {
 			return null;
 		}
+	}
+
+	public String rankingVotacaoPorPalestra(Palestra palestra) {
+		String jpql = "select sum(v.voto) from Votacao v where v.palestra_id = :pPalestra and v.tipoVoto = :pTipoVoto";
+		TypedQuery<Long> query = this.manager.createQuery(jpql, Long.class).setParameter("pPalestra", palestra)
+				.setParameter("pTipoVoto", VotacaoEnum.POSITIVO);
+		Long votosP = query.getSingleResult();
+		query.setParameter("pTipoVoto", VotacaoEnum.NEGATIVO);
+		Long votosN = query.getSingleResult();
+
+		BigDecimal votosPositivos = (votosP == null ? new BigDecimal(0) : BigDecimal.valueOf(votosP));
+		BigDecimal votosNegativos = (votosN == null ? new BigDecimal(0) : BigDecimal.valueOf(votosN));
+
+		if ((votosPositivos.compareTo(new BigDecimal(0)) == 0) && (votosNegativos.compareTo(new BigDecimal(0)) == 0)) {
+			return "NAO AVALIADA";
+		}
+
+		BigDecimal totalVotos = votosPositivos.add(votosNegativos);
+		BigDecimal porcentagemPositivo = votosPositivos.divide(totalVotos, 2, RoundingMode.HALF_EVEN)
+				.multiply(new BigDecimal(100));
+
+		if ((totalVotos.compareTo(new BigDecimal(50)) >= 0) && ((porcentagemPositivo.compareTo(new BigDecimal(51)) >= 0)
+				&& (porcentagemPositivo.compareTo(new BigDecimal(55)) <= 0))) {
+			return "POLEMICA";
+		}
+
+		if ((totalVotos.compareTo(new BigDecimal(100)) > 0)
+				&& (porcentagemPositivo.compareTo(new BigDecimal(50)) >= 1)) {
+			return "APROVADA";
+		} else {
+			if ((votosNegativos.compareTo(new BigDecimal(0)) > 0)) {
+				if (votosPositivos.divide(votosNegativos, 2, RoundingMode.HALF_EVEN)
+						.compareTo(new BigDecimal(2)) >= 0) {
+					return "APROVADA";
+				} else {
+					return "POLEMICA";
+				}
+			} else {
+				return "APROVADA";
+			}
+		}
+
 	}
 
 }
