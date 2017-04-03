b2774d3d94cc4eba8d9565b2404c1769eb2b4beb
rdv_consulting <r.rodrigues78@yahoo.com.br>
2017-04-03 00:50:56 -0300

    Criacao do Metodo getPalestra() na Classe PalestraDAO.

--- src/main/java/br/com/caelum/evento/dao/PalestraDAO.java
+++ src/main/java/br/com/caelum/evento/dao/PalestraDAO.java
@@ -2,13 +2,20 @@ package br.com.caelum.evento.dao;
 
 import java.io.Serializable;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
 
+import br.com.caelum.evento.domain.Evento;
 import br.com.caelum.evento.domain.Palestra;
 
 public class PalestraDAO extends GenericDAO<Palestra> implements Serializable {
 
-	private static final long serialVersionUID = 6915947383465602558L;
+	private static final long serialVersionUID = 2040427863328163967L;
+
+	@Inject
+	private EntityManager manager;
 
 	public PalestraDAO() {
 		super(Palestra.class);
@@ -16,6 +23,18 @@ public class PalestraDAO extends GenericDAO<Palestra> implements Serializable {
 
 	public PalestraDAO(EntityManager manager) {
 		super(Palestra.class, manager);
+	}
+
+	public Palestra getPalestra(Evento evento, String titulo) {
+		try {
+			TypedQuery<Palestra> query = this.manager
+					.createQuery("select p from Palestra p where p.evento = :pEvento and p.titulo = :pPalestra",
+							Palestra.class)
+					.setParameter("pEvento", evento).setParameter("pPalestra", titulo);
+			return query.getSingleResult();
+		} catch (NoResultException nre) {
+			return null;
+		}
 	}
 
 }
