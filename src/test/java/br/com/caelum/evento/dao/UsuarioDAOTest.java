ab8a100432181cb2aad7be9352feb7fd6c79a82b
rdv_consulting <r.rodrigues78@yahoo.com.br>
2017-04-04 16:15:02 -0300

    Refatoracao da Classe de Teste UsuarioDAOTest.

--- src/test/java/br/com/caelum/evento/dao/UsuarioDAOTest.java
+++ src/test/java/br/com/caelum/evento/dao/UsuarioDAOTest.java
@@ -20,10 +20,7 @@ public class UsuarioDAOTest {
 	private static EntityManagerFactory emf;
 	private EntityManager manager;
 
-	private String nomeUsuario;
-	private boolean excluiUsuario = true;
-
-	private Usuario usuario = new Usuario();
+	private Usuario usuario;
 
 	private UsuarioDAO usuarioDAO;
 
@@ -50,20 +47,14 @@ public class UsuarioDAOTest {
 		emf.close();
 	}
 
-	private void setUsuario(Usuario usr) {
+	private String randomNumber() {
 		Random rdm = new Random();
-		Integer inteiro = rdm.nextInt(1000);
-		this.nomeUsuario = "TESTE_" + inteiro.toString();
-		usr.setNome(this.nomeUsuario);
-		usr.setEmail("teste@teste.com");
-		usr.setSenha("123456");
+		Integer inteiro = rdm.nextInt();
+		return inteiro.toString();
 	}
 
-	private void excluirUsuario() {
-		if (this.usuario != null) {
-			usuarioDAO.remove(this.usuario);
-		}
-		this.usuario = new Usuario();
+	private void setUsuario(Usuario usr) {
+		this.usuario = new Usuario("TESTE_" + randomNumber(), "teste@teste.com", "123");
 	}
 
 	@Test
@@ -71,51 +62,45 @@ public class UsuarioDAOTest {
 		this.setUsuario(this.usuario);
 		this.usuarioDAO.adiciona(this.usuario);
 		Assert.assertNotNull(this.usuario.getId());
-		if (this.excluiUsuario == true) {
-			this.excluirUsuario();
-		}
 	}
 
 	@Test
 	public void deveAlterarEmaildoUsuario() {
-		this.excluiUsuario = false;
 		this.deveInserirUsuario();
-		this.usuario.setEmail("teste@teste.com");
+		this.usuario.setEmail("altera@teste.com");
 		this.usuarioDAO.altera(this.usuario);
-		Assert.assertEquals("teste@teste.com", this.usuario.getEmail());
-		this.excluirUsuario();
+		Assert.assertEquals("altera@teste.com", this.usuario.getEmail());
 	}
 
 	@Test
 	public void deveExcluirUsuario() {
-		this.excluiUsuario = false;
 		this.deveInserirUsuario();
 		this.usuarioDAO.remove(this.usuario);
-		Assert.assertNull(this.usuarioDAO.buscaString(this.nomeUsuario, "nome"));
+		Assert.assertNull(this.usuarioDAO.buscaString(this.usuario.getNome(), "nome"));
 	}
 
 	@Test
 	public void deveBuscarIdUsario() {
-		this.excluiUsuario = false;
 		this.deveInserirUsuario();
 		Assert.assertNotNull(this.usuarioDAO.buscaId(this.usuario.getId()));
-		this.excluirUsuario();
 	}
 
 	@Test
 	public void deveBuscarStringNomeUsuario() {
-		this.excluiUsuario = false;
 		this.deveInserirUsuario();
-		Assert.assertNotNull(this.usuarioDAO.buscaString(this.nomeUsuario, "nome"));
-		this.excluirUsuario();
+		Assert.assertNotNull(this.usuarioDAO.buscaString(this.usuario.getNome(), "nome"));
 	}
 
 	@Test
 	public void deveListarUsuarios() {
-		this.excluiUsuario = false;
 		this.deveInserirUsuario();
 		Assert.assertNotEquals(0, this.usuarioDAO.lista().size());
-		this.excluirUsuario();
+	}
+
+	@Test
+	public void deveEncontrarUsuario() {
+		this.deveInserirUsuario();
+		Assert.assertNotNull(this.usuarioDAO.existeUsuario(this.usuario));
 	}
 
 }
