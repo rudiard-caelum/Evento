5d847e14c140cde8f0d5d668dfe3e52c7d3d5c81
rdv_consulting <r.rodrigues78@yahoo.com.br>
2017-04-07 00:31:03 -0300

    Refatoracao do Nome dos Metodos e Criacao do Metodo ListaComentariosPalestra() da Classe ComentarioDAO.

--- src/main/java/br/com/caelum/evento/dao/ComentarioDAO.java
+++ src/main/java/br/com/caelum/evento/dao/ComentarioDAO.java
@@ -9,13 +9,14 @@ import javax.persistence.NoResultException;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
-import br.com.caelum.evento.domain.Palestra;
 import br.com.caelum.evento.domain.Comentario;
+import br.com.caelum.evento.domain.Palestra;
 import br.com.caelum.evento.domain.Usuario;
 
 public class ComentarioDAO extends GenericDAO<Comentario> implements Serializable {
 
-	private static final long serialVersionUID = 1389354249828845021L;
+	private static final long serialVersionUID = -1504646177514130696L;
+
 	@Inject
 	private EntityManager manager;
 
@@ -29,7 +30,7 @@ public class ComentarioDAO extends GenericDAO<Comentario> implements Serializabl
 	}
 
 	@SuppressWarnings("unchecked")
-	public List<Palestra> listaComentario(Usuario usuario, String tipo) {
+	public List<Palestra> listaComentarioUsuario(Usuario usuario, String tipo) {
 		Query query = this.manager.createNamedQuery("PalestraComentario." + tipo, Palestra.class)
 				.setParameter("pUsuario", usuario);
 		List<Palestra> lista = query.getResultList();
@@ -38,21 +39,30 @@ public class ComentarioDAO extends GenericDAO<Comentario> implements Serializabl
 
 	public boolean buscaComentarioUsuario(Usuario usuario, Palestra palestra) {
 		Query query = this.manager
-				.createQuery(
-						"select c from Comentario c where c.usuario = :pUsuario and c.palestra = :pPalestra")
+				.createQuery("select c from Comentario c where c.usuario = :pUsuario and c.palestra = :pPalestra")
 				.setParameter("pUsuario", usuario).setParameter("pPalestra", palestra);
 		boolean encontrado = !query.getResultList().isEmpty();
 		return encontrado;
 	}
 
-	public Comentario getComentario(Usuario usuario, Palestra palestra) {
+	public Comentario getComentarioUsuario(Usuario usuario, Palestra palestra) {
 		try {
 			TypedQuery<Comentario> query = this.manager
-					.createQuery(
-							"select c from Comentario c where c.usuario = :pUsuario and c.palestra = :pPalestra",
+					.createQuery("select c from Comentario c where c.usuario = :pUsuario and c.palestra = :pPalestra",
 							Comentario.class)
 					.setParameter("pUsuario", usuario).setParameter("pPalestra", palestra);
 			return query.getSingleResult();
+		} catch (NoResultException nre) {
+			return null;
+		}
+	}
+
+	public List<Comentario> listaComentariosPalestra(Palestra palestra) {
+		try {
+			TypedQuery<Comentario> query = this.manager
+					.createQuery("select c from Comentario c where c.palestra = :pPalestra", Comentario.class)
+					.setParameter("pPalestra", palestra);
+			return query.getResultList();
 		} catch (NoResultException nre) {
 			return null;
 		}
